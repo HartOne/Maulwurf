ae431f0e83dac79b627ffeb5f193de2e4047f49c
ah <ah@KIWIPLUS>
2016-10-08 21:05:37 +0200

    end of session 3 "Maulwurf wegklicken und Punkte Zählen"

--- Maulwurf/MainGame.xaml.cs
+++ Maulwurf/MainGame.xaml.cs
@@ -22,9 +22,86 @@ namespace Maulwurf
     /// </summary>
     public sealed partial class MainGame : Page
     {
+        private Moles[] mole = new Moles[9];
+        private Player player;
+        private DispatcherTimer gameTimer;
+
         public MainGame()
         {
             this.InitializeComponent();
+            InitGame();
+            InsertNewMole();
         }
+
+        private void InsertNewMole()
+        {
+            int pos = 8;
+            mole [pos].Generate(pos);
+            mole[pos].Img.Tag = mole[pos];
+            mole[pos].Img.Tapped += Img_Tapped;
+            gameField.Children.Add(mole[pos].Img);
+        }
+
+        private void InitGame()
+        {
+            for (int i = 0; i < mole.Length; i++)
+            {
+                mole[i] = new Moles () ;
+            }
+            player = new Player { Score = 0 };
+            ScoreText.Text = player.Score.ToString();
+            gameTimer = new DispatcherTimer();
+            gameTimer.Interval = new TimeSpan(0, 0, 1);
+            gameTimer.Tick += GameTimer_Tick;
+            gameTimer.Start();
+        }
+
+        private void GameTimer_Tick(object sender, object e)
+        {
+            CheckallMoles();
+        }
+
+        private void CheckallMoles()
+        {
+            for (int i = 0; i < mole.Length; i++) 
+            {
+                if (mole[i].Timer>= 0)
+                {
+                    mole[i].Timer -= 1;
+                }
+                else
+                {
+                    if (mole[i].Img!= null)
+                    {
+                        gameField.Children.Remove(mole[i].Img);
+                        mole[i].Img = null;
+                        mole[i].Timer = -1;
+                    }
+                }
+            }
+        }
+
+        private void AddScore(int scoreValue)
+        {
+            player.Score += scoreValue;
+            ScoreText.Text = player.Score.ToString();
+        }
+
+        private void Img_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            var img = sender as Image;
+            if (img == null)
+
+            {
+                return;
+            }
+            var tempMole = img.Tag as Moles;
+            if (tempMole == null)
+            {
+                return;
+            }
+            AddScore(1);
+            gameField.Children.Remove(tempMole.Img);
+           }
     }
 }
